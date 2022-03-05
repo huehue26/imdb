6046ff0e3f4b9829ea80a1b171351f9f49e4f0c6
Somesh Singh <someshsingh298@gmail.com>
2022-03-05 14:44:39 +0530

    final commit

--- firebase-config.jsx
+++ firebase-config.jsx
@@ -2,37 +2,14 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const app = firebase.initializeApp({
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MEASURMENT_ID
-// });
-
-// console.log(process.env.FIREBASE_API_KEY)
-// const x = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MEASURMENT_ID
-// };
-// const app = firebase.initializeApp(x)
-// console.log(x);
-
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCEHGtH-wOr0PPt8oKSe7iP7u8JEl4TzKA",
-  authDomain: "imdb-bd948.firebaseapp.com",
-  projectId: "imdb-bd948",
-  storageBucket: "imdb-bd948.appspot.com",
-  messagingSenderId: "780788773933",
-  appId: "1:780788773933:web:2c4b0129faeab3e88f34a4",
-  measurementId: "G-HWK2B1LYNV",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASURMENT_ID,
 });
 
 export const auth = app.auth();
